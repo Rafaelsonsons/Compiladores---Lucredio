ef3139048732498f83550e1fd70a53c6c28c8c66
FYuyaS <119973346+FYuyaS@users.noreply.github.com>
2025-05-28 23:55:38 -0300

    Add files via upload

---
+++ T4Teste/src/main/java/br/ufscar/dc/compiladores/expr/parser/Principal.java
@@ -0,0 +1,43 @@
+package br.ufscar.dc.compiladores.expr.parser;
+import br.ufscar.dc.compiladores.expr.parser.AlgumaParser.ProgramaContext;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.Token;
+
+public class Principal {
+    public static void main(String args[]) throws IOException {
+
+        String arquivoSaida = args[1];
+
+        try(PrintWriter pw = new PrintWriter(arquivoSaida)) {
+            CharStream cs = CharStreams.fromFileName(args[0]);
+
+            AlgumaLexer lexer = new AlgumaLexer(cs);
+
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+            AlgumaParser parser = new AlgumaParser(tokens);
+
+            ProgramaContext arvore = parser.programa();
+
+            AlgumaSemantico as = new AlgumaSemantico();
+
+            as.visitPrograma(arvore);
+
+            for (String error : AlgumaSemanticoUtils.errosSemanticos){
+                pw.println(error);
+            }
+            pw.println("Fim da compilacao");
+        } catch(FileNotFoundException fnfe) {
+            System.err.println("O arquivo/diretório não existe:"+args[1]);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
